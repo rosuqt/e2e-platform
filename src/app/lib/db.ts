af357b98499acc88a7da7ebe4f83da19964ffe4b
rosuqt <allyzarosecayer@gmail.com>
2025-04-17 04:21:38 +0800

    test: .env update3

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -4,11 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || "5432", 10),
+  connectionString: process.env.DATABASE_URL,
 });
 
 client.connect();
