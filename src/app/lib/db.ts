0b488d48ae1ff71b243e6cb359745f4c812cf5b9
rosuqt <allyzarosecayer@gmail.com>
2025-04-17 02:59:09 +0800

    test: supabase connection

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,11 +1,14 @@
 import { Client } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "e2e_auth",
-  password: "Paramore867",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432", 10),
 });
 
 client.connect();
