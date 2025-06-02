02c19c2de48493b7d86660bab5cb2f5fd2c11f88
rosuqt <allyzarosecayer@gmail.com>
2025-06-02 23:47:51 +0800

    Deployment | Test v4

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,4 +1,6 @@
-import MainLanding from "./landing/main-landing";
+import dynamic from 'next/dynamic';
+
+const MainLanding = dynamic(() => import('./landing/main-landing'), { ssr: false });
 
 export default async function LandingPage() {
   await new Promise((resolve) => setTimeout(resolve, 3000));
