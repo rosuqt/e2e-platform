ad007c8ae54decc8629c4b7e3bbb61d5d9c03e26
rosuqt <allyzarosecayer@gmail.com>
2025-06-02 22:43:45 +0800

    Deployment | v2 - Initial release

--- app/hooks/useNotifications.ts
+++ app/hooks/useNotifications.ts
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import supabase from '@/lib/supabase';
 import { toast } from 'react-hot-toast';
 
 export function useNotifications() {
