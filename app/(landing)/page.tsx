aed2699e8a93c00bfa6f2dddc17fc33f46fce2d7
rosuqt <allyzarosecayer@gmail.com>
2025-06-03 00:03:03 +0800

    Deployment | Test v6

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import dynamic from 'next/dynamic';
-
-const MainLanding = dynamic(() => import('./landing/main-landing'), { ssr: false });
+import MainLanding from './landing/main-landing';
 
 export default function LandingPage() {
   return <MainLanding />;
