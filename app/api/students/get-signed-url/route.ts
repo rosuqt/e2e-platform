dd85670ce070fbba304f332d46ce89a1997b86ad
rosuqt <allyzarosecayer@gmail.com>
2025-06-05 08:34:55 +0800

    Deployment | Test v10.1

--- app/api/students/get-signed-url/route.ts
+++ app/api/students/get-signed-url/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import supabase, { getAdminSupabase } from "@/lib/supabase";
+import { getAdminSupabase } from "@/lib/supabase";
 
 export async function POST(req: NextRequest) {
   try {
